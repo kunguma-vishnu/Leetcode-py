bce1bef6131c2188f2d73ebcb77b136765dbd030
Kunguma Vishnu <vishnudazzler7@gmail.com>
2023-10-06 10:01:54 +0530

    Time: 40 ms (59.64%), Space: 16.3 MB (27.37%) - LeetHub

--- 0343-integer-break/0343-integer-break.py
+++ 0343-integer-break/0343-integer-break.py
@@ -1,13 +1,11 @@
 class Solution:
     def integerBreak(self, n: int) -> int:
         if n<=3: return n-1
-        dp = [0] * (n+1)
-        dp[1] = 1
-        dp[2] = 2
-        dp[3] = 3
-        for i in range(4,n+1):
-            cur = 0
-            for j in range(1,i//2+1):
-                cur = max(cur,dp[j]*dp[i-j])
-            dp[i] = cur
-        return dp[n]
+        quotient = n//3
+        product = 3**quotient
+        if n%3 == 1:
+            product //= 3
+            product *= 4
+        elif n%3 == 2:
+            product *= 2
+        return product
