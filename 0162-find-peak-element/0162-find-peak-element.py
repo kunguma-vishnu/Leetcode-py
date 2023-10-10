8d66121e07d117cac98653672e88bff3ecbc4a8b
Kunguma Vishnu <vishnudazzler7@gmail.com>
2023-10-10 20:02:19 +0530

    Time: 58 ms (14.8%), Space: 16.5 MB (45.85%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.py
+++ 0162-find-peak-element/0162-find-peak-element.py
@@ -1,3 +1,9 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        return nums.index(max(nums))
+        ind = 0
+        element = nums[0]
+        for i in range(1,len(nums)):
+            if nums[i] > element:
+                ind = i
+                element = nums[i]
+        return ind
