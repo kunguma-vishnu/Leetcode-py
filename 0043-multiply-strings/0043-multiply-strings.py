c282b7bcf54a77c1bf35f05e39e87e07d07bf2e8
Kunguma Vishnu <vishnudazzler7@gmail.com>
2023-10-14 18:20:42 +0530

    Time: 164 ms (10.77%), Space: 16.3 MB (60.64%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.py
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -1,3 +1,7 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        return str(int(num1)*int(num2))
+        res, pos = 0, len(num1 + num2) - 2
+        for i1, n1 in enumerate(map(int, num1)):
+            for i2, n2 in enumerate(map(int, num2)):
+                res += n1 * n2 * 10 ** (pos - (i1 + i2))
+        return str(res)
